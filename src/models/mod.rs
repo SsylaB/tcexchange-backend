d856050b5d35a6f2bec464353910eb11b7bbe2a9
aidaabderrazik-netizen <aida.abderrazik@gmail.com>
2026-03-26 13:39:55 +0100

    Update mod.rs

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Clone, sqlx::FromRow)]
+#[derive(Serialize, Deserialize, Clone, sqlx::FromRow, Debug)] 
 pub struct Destination {
     pub id: i64,
     pub university_name: String,
@@ -8,7 +8,7 @@ pub struct Destination {
     pub location: Option<String>,
     pub url: Option<String>,
     pub exchange_type: Option<String>,
-    pub languages: Option<String>,  // comma-separated string
+    pub languages: Option<String>,
     pub description: Option<String>,
     pub short_name: Option<String>,
     pub position: Option<String>,
