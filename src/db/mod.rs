c9c3535b37323da57fe4b9e674626f115d99b04a
aidaabderrazik-netizen <aida.abderrazik@gmail.com>
2026-03-26 13:39:03 +0100

    Update mod.rs

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,9 +1,15 @@
-use sqlx::{SqlitePool, sqlite::SqlitePoolOptions};
+use sqlx::{SqlitePool, sqlite::{SqlitePoolOptions, SqliteConnectOptions}};
+use std::str::FromStr;
 
 pub async fn create_pool() -> SqlitePool {
+    // On configure les options de connexion pour autoriser la création du fichier
+    let connection_options = SqliteConnectOptions::from_str("sqlite:tcexchange.db")
+        .unwrap()
+        .create_if_missing(true); // <--- L'option magique est ici
+
     SqlitePoolOptions::new()
         .max_connections(5)
-        .connect("sqlite:./tcexchange.db")
+        .connect_with(connection_options)
         .await
         .expect("Failed to connect to SQLite")
 }
